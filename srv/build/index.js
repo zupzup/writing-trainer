a52757c6f50daaaf921a986c18f7933dfdb15208
Mario Zupan <mario@zupzup.org>
2022-02-14 18:02:12 +0100

    api

--- srv/build/index.js
+++ srv/build/index.js
@@ -18,19 +18,48 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importStar(require("express"));
-const cors = require('cors');
+const cors_1 = __importDefault(require("cors"));
+const fs = __importStar(require("fs"));
+const de_words = JSON.parse(fs.readFileSync('./languages/de.json', { encoding: 'utf8', flag: 'r' }));
+const en_words = JSON.parse(fs.readFileSync('./languages/en.json', { encoding: 'utf8', flag: 'r' }));
 const app = (0, express_1.default)();
 const route = (0, express_1.Router)();
-route.get("/", async (req, res) => {
-    console.log("hello");
+route.get("/words/de/:num", async (req, res) => {
+    const num = parseInt(req.params.num);
     return res.json({
-        message: "Hello!"
+        words: n_random_words_from_file(de_words, num),
     });
 });
+route.get("/words/en/:num", async (req, res) => {
+    const num = parseInt(req.params.num);
+    return res.json({
+        words: n_random_words_from_file(en_words, num),
+    });
+});
+function rand(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+function n_random_words_from_file(file, num) {
+    const len = file.length;
+    const random_nums = [];
+    let i = 0;
+    while (i < num) {
+        let new_rand = rand(0, len);
+        if (random_nums.includes(new_rand)) {
+            continue;
+        }
+        i++;
+        random_nums.push(new_rand);
+    }
+    return random_nums.map((n) => file[n]);
+}
 app.use(express_1.default.json());
-app.use(cors());
+app.use((0, cors_1.default)());
 app.use(route);
 app.listen(8080, () => {
     console.log("Server running on port 8080");
--- srv/src/index.ts
+++ srv/src/index.ts
@@ -1,17 +1,49 @@
 import express, { Application, Router, Request, Response } from 'express';
+import cors from 'cors';
+import * as fs from 'fs';
 
-const cors = require('cors');
-
+const de_words: String[] = JSON.parse(fs.readFileSync('./languages/de.json', {encoding:'utf8', flag:'r'}));
+const en_words: String[] = JSON.parse(fs.readFileSync('./languages/en.json', {encoding:'utf8', flag:'r'}));
 
 const app: Application = express();
 const route = Router();
 
-route.get("/", async(req: Request, res: Response): Promise<any> => {
-    console.log("hello");
+route.get("/words/de/:num", async(req: Request, res: Response): Promise<any> => {
+    const num: number = parseInt(req.params.num);
     return res.json({
-        message: "Hello!"
+        words: n_random_words_from_file(de_words, num),
     });
 });
+
+route.get("/words/en/:num", async(req: Request, res: Response): Promise<any> => {
+    const num: number = parseInt(req.params.num);
+    return res.json({
+        words: n_random_words_from_file(en_words, num),
+    });
+});
+
+function rand(min: number, max: number) {
+    return Math.floor(
+        Math.random() * (max - min) + min
+    )
+}
+
+function n_random_words_from_file(file: String[], num: number): String[] {
+    const len = file.length;
+    const random_nums: number[] = [];
+    let i = 0;
+
+    while (i < num) {
+        let new_rand = rand(0, len);
+        if (random_nums.includes(new_rand)) {
+            continue;
+        }
+        i++;
+        random_nums.push(new_rand);
+    }
+    return random_nums.map((n) => file[n])
+}
+
 
 app.use(express.json());
 app.use(cors());
