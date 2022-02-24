7127b62b5d7d3939e0f56d912ac69003f53f7ff6
Mario Zupan <mario@zupzup.org>
2022-02-24 18:35:21 +0100

    add num error handling

--- srv/src/index.ts
+++ srv/src/index.ts
@@ -5,11 +5,21 @@ import * as fs from 'fs';
 const de_words: String[] = JSON.parse(fs.readFileSync('./languages/de.json', {encoding:'utf8', flag:'r'}));
 const en_words: String[] = JSON.parse(fs.readFileSync('./languages/en.json', {encoding:'utf8', flag:'r'}));
 
+const MAX_NUM: number = 100;
+
 const app: Application = express();
 const route = Router();
 
 route.get("/words/de/:num", async(req: Request, res: Response): Promise<any> => {
     const num: number = parseInt(req.params.num);
+
+    if (num > 100 || num < 1) {
+        res.status(400);
+        return res.json({
+            error: "number needs to be between 1 and 100"
+        })
+    }
+
     return res.json({
         words: n_random_words_from_file(de_words, num),
     });
@@ -17,6 +27,14 @@ route.get("/words/de/:num", async(req: Request, res: Response): Promise<any> => 
 
 route.get("/words/en/:num", async(req: Request, res: Response): Promise<any> => {
     const num: number = parseInt(req.params.num);
+
+    if (num > 100 || num < 1) {
+        res.status(400);
+        return res.json({
+            error: "number needs to be between 1 and 100"
+        })
+    }
+
     return res.json({
         words: n_random_words_from_file(en_words, num),
     });
